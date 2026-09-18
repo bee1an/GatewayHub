155fc8e442903166762bd22e6400e3afc670c6de
bee <beelan@yeah.net>
2026-09-18 17:32:12 +0800

    feat: shared marquee text

--- src/root.rs
+++ src/root.rs
@@ -21,9 +21,9 @@ use gateway_core::{
 };
 
 pub(crate) use chrome::{
-    MONO, card, card_rows, card_uniform_list, clock_time, enter, hairline, one_line, page_header,
-    pop_in, provider_logo, row, section_header, shake, short_date, skeleton_rows, status_label,
-    toggle_filter,
+    MONO, MarqueeText, card, card_rows, card_uniform_list, clock_time, enter, hairline, one_line,
+    page_header, pop_in, provider_logo, row, section_header, shake, short_date, skeleton_rows,
+    status_label, toggle_filter,
 };
 
 use gpui_kit::assets::IconName;
